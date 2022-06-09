2b98d905f9dfd28e72d93ad37811b9738f665410
Passenger89 <nicho268@gmail.com>
2022-06-09 17:24:16 -0500

    update FeatureCard.jsx

--- src/components/FeaturedCard/FeaturedCard.jsx
+++ src/components/FeaturedCard/FeaturedCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './FeaturedCard.scss'
 
-const FeaturedCard = ({ title, description }) => {
+const FeaturedCard = ({ title, description, image }) => {
+  console.log(image)
   return (
     <div className='card flex column br-2'>
-      <div className='image'>
+      <div className='image' style={{ backgroundImage: `url('${image}')` }}>
         <h4 className='uppercase fw-800 clr-white'>featured</h4>
       </div>
       <div className='content gap'>
--- src/components/FeaturedSection/FeaturedSection.jsx
+++ src/components/FeaturedSection/FeaturedSection.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import FeaturedCard from '../FeaturedCard/FeaturedCard.jsx'
 import './FeaturedSection.scss'
+import mellowRoast from '../../assets/images/beans/Beans - mellow.jpg'
+import darkRoast from '../../assets/images/beans/Beans - dark.jpg'
+import citrusRoast from '../../assets/images/beans/Beans - citrus.jpg'
 
 const FeaturedSection = () => {
   return (
     <section className='section flex column gap'>
       <FeaturedCard
+        image={mellowRoast}
         title='Mellow Roast'
         description='Hints of caramel and notes of chocolate.'
       />
       <FeaturedCard
+        image={darkRoast}
         title='Dark Roast'
         description='Rich and deep. For the devotees looking for a coffee that can touch the soul and soothe the heart.'
       />
       <FeaturedCard
+        image={citrusRoast}
         title='Citrus Roast'
         description='Fruity. A blackcurrant undertone with subtle notes of cranberry.'
       />
